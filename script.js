ba89b7c5201286e5238ce0712feb0da61a9fbd05
Petre Andrei Tiberius <129947125+TsRo6@users.noreply.github.com>
2023-05-26 17:07:59 +0300

    pana la d

--- script.js
+++ script.js
@@ -22,18 +22,47 @@ freeDogsSpace();
 
 // b) stiind ca hotelul poate primi maxim 30 de animale in acelasi timp, implementati o conditie care verifica daca hotelul este supra-aglomerat
 
+var maxPets = 30;
 
+var allPets = numberOfDogs + numberOfCats;
+
+function hotelSpace(){
+    if(allPets < maxPets){
+        console.log('The hotel is not full, there are still free places:', maxPets - allPets);
+    } else {
+        console.log('The hotel is full');
+    }
+}
+
+hotelSpace();
 
 
 
 // c) pentru fiecare pisica trebuie sa fie disponibile cate 3 jucarii, iar pentru fiecare caine cate 5. Calculati si afisati cate jucarii trebuie sa cumpere cei de la hotel pentru animale.
 
+var maxNumberOfCats = maxPets - maxNumberOfDogs;
  
 
+function totalToys(){
+    var numberOfCatToys = 3 * maxNumberOfCats;
+    var numberOfDogToys = 5 * maxNumberOfDogs;
+    var totalPetToys = 3 * maxNumberOfCats + 5 * maxNumberOfDogs;
+    console.log('Total cat toys:', numberOfCatToys, 'Total dog toys:', numberOfDogToys, 'Total pet toys:', totalPetToys);
+}
+
+totalToys();
 
 // d) daca numarul total al animalelor este mai mic de 20 sau sunt mai multi de 5 angajati, consideram ca animalele sunt bine ingrijite, implementati o conditie care sa verifice si sa afiseze in consola mesaje corespunzatoare
 
+function petsCare(){
+    if((maxPets < 20) || (numberOfEmployees > 5)){
+        console.log('We will take cared for your pet');
+    } else {
+        console.log('Sorry, no caregiver is available');
+    }
+}
 
+petsCare()
 
 // e) numarul norocos al patronului este 7. Daca la orice moment cand viziteaza hotelul (este si el acolo!) numarul total al fiintelor din interior este multiplu de 7, fiecare angajat va primi un bonus de 100 de lei. Calculati si afisati in consola cati bani il va costa o astfel de vizita.
 
